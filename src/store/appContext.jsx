a680cad44cb6fb381a7c07c8d2eb1bce6917e26d
FbcGa <fabricio.alipazaga.pe@gmail.com>
2025-01-25 17:37:45 -0500

    fixed get list

--- src/store/appContext.jsx
+++ src/store/appContext.jsx
@@ -1,27 +1,29 @@
 import { useState, useEffect, createContext } from "react";
-import getState from "./flux"
+import getState from "./flux";
 
 export const Context = createContext();
 
-const StoreWrapper = ({children}) => {
-  const [state, setState] = useState(getState({
-    getStore: () => state.store,
-    getActions: () => state.actions,
-    setStore: (updatedStore) => setState({
-      store: Object.assign(state.store, updatedStore),
-      actions: { ...state.actions }
+const StoreWrapper = ({ children }) => {
+  const [state, setState] = useState(
+    getState({
+      getStore: () => state.store,
+      getActions: () => state.actions,
+      setStore: (updatedStore) =>
+        setState({
+          store: Object.assign(state.store, updatedStore),
+          actions: { ...state.actions },
+        }),
     })
-  }));
+  );
 
   useEffect(() => {
-    state.actions.getMessage();
+    const getAllList = async () => {
+      state.actions.allList();
+    };
+    getAllList();
   }, []);
 
-  return (
-    <Context.Provider value={state}>
-      {children}
-    </Context.Provider>
-  );
+  return <Context.Provider value={state}>{children}</Context.Provider>;
 };
 
 export default StoreWrapper;
